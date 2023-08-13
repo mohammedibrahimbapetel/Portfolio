a2ddc5c220c9b05452e911058594ff93357c8a2c
Mohammed <muaminab@gmail.com>
2023-08-13 10:09:06 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="logo">
         <a href="">
-          <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo" /> 
         </a>
       </div>
       <div className="btn">
